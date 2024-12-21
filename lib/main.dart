b7c673ace157a159015a750b07801849f32fc97b
alexcao <alexcao194@gmail.com>
2024-12-21 21:14:21 +0700

    chore(android): add flavor, app name and app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 import 'configs/di.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
   await DI().init();
   runApp(const App());
 }
