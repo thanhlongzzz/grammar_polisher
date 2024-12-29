7484d28985a481f39de6be2831dcf37a701a2086
alexcao <alexcao194@gmail.com>
2024-12-29 22:41:53 +0700

    feat(review): add review screen

--- lib/ui/screens/review/review_screen.dart
+++ lib/ui/screens/review/review_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ReviewScreen extends StatelessWidget {
+import '../../../data/models/word_status.dart';
+import '../../commons/base_page.dart';
+import '../vocabulary/bloc/vocabulary_bloc.dart';
+import '../vocabulary/widgets/vocabulary_item.dart';
+import 'widgets/empty_review_page.dart';
+
+class ReviewScreen extends StatefulWidget {
   const ReviewScreen({super.key});
 
   @override
+  State<ReviewScreen> createState() => _ReviewScreenState();
+}
+
+class _ReviewScreenState extends State<ReviewScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final vocabularyState = context.watch<VocabularyBloc>().state;
+    final reviewWords = vocabularyState.words.where((word) => word.status == WordStatus.star).toList();
+    return BasePage(
+      title: 'Review',
+      child: reviewWords.isNotEmpty
+          ? ListView.builder(
+              itemCount: reviewWords.length,
+              itemBuilder: (context, index) {
+                final word = reviewWords[index];
+                return VocabularyItem(word: word);
+              },
+            )
+          : EmptyReviewPage(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<VocabularyBloc>().add(const VocabularyEvent.getAllOxfordWords());
   }
 }
---
+++ lib/ui/screens/review/widgets/empty_review_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../generated/assets.dart';
+import '../../../../navigation/app_router.dart';
+import '../../../commons/rounded_button.dart';
+
+class EmptyReviewPage extends StatelessWidget {
+  const EmptyReviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'No words to review',
+          style: textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 16),
+        Image.asset(
+          Assets.pngBook,
+        ),
+        const SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            'You can add some words to your review list and use flashcards to memorize them.',
+            style: textTheme.bodyMedium?.copyWith(
+              color: colorScheme.onSurface.withAlpha(150),
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        const SizedBox(height: 8),
+        RoundedButton(
+          expand: false,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          borderRadius: 8,
+          onPressed: () => _onAddWords(context),
+          child: Text(
+            'Add words to review',
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onAddWords(BuildContext context) {
+    context.go(RoutePaths.vocabulary);
+  }
+}
